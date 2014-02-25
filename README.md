979ceb4934d7fc94df94b1875d0d7a96288ed515
John Jensen MBP <johncjensen@hotmail.com>
2014-02-24 18:41:59 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Merge Sort
+Ruby implementation of computer science merge sort algorithm.
+
+[Merge Sort Info](http://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Merge_sort#Ruby)
+
+####How to Run
+
+  ruby ms.rb
+#####[MIT License](http://johnjensen.mit-license.org)
+
+![alt text](http://upload.wikimedia.org/wikipedia/commons/c/cc/Merge-sort-example-300px.gif)
+
+
+
---
+++ ms.rb
@@ -0,0 +1,41 @@
+require 'benchmark'
+
+def merge_sort(list)
+  return list if list.size <= 1
+
+  middle = list.size / 2
+  left = list[0,middle]
+  right = list[middle..-1]
+  merge(merge_sort(left), merge_sort(right))
+end
+
+def merge(left, right)
+  result = []
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      result << left.shift
+    else
+      result << right.shift
+    end
+  end
+  result + left + right
+end
+
+randomarray = (0...10000).to_a.sample 10
+p merge_sort(randomarray)
+
+
+#### Benchmarking tests
+
+# slower = Benchmark.realtime do
+# randomarray1 = (0...1000000).sort_by{rand}[0...10]
+# p randomarray1
+# end
+# puts slower
+
+
+# faster = Benchmark.realtime do
+# randomarray = (0...1000000).to_a.sample 10
+# p randomarray
+# end
+# puts faster
